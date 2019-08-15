0d027042f087363413fe199d28a32f03abaf0ae6
Filipe Fernandes <ocefpaf@gmail.com>
2019-08-15 11:58:06 -0300

    re-render, try new commit, re-try tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -25,10 +25,8 @@ export CPPBIN="${CPP}"
 
 make
 
-# Only one test is failing on macOS and Linux.
-# commenting this for now until we have a new release.
-# start_db
-# make check
-# stop_db
+start_db
+make check
+stop_db
 
 make install
